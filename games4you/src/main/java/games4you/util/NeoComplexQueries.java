6b28feabaaf3f2486174c2691b07d3c8ca955ebc
iacopo <iacopo.canetta@gmail.com>
2024-04-11 10:48:45 +0200

    query modified

--- games4you/src/main/java/games4you/util/NeoComplexQueries.java
+++ games4you/src/main/java/games4you/util/NeoComplexQueries.java
@@ -7,15 +7,17 @@ public class NeoComplexQueries {
         //create relationships between review, game and gamer
         Neo4jManager neo4j = Neo4jManager.getInstance();
         String query = String.format(
-                "MATCH (u:User {id: %d})-[:FRIEND]-(friend:User)"+
-                "WITH u, collect(DISTINCT friend) AS friends)" +
-                "MATCH (u)-[:OWNS]->(game:Game)<-[HAS_REVIEW]-(r:Review)<-[HAS_WROTE]-(reviewer)" +
-                "WHERE reviewer IN friends" +
-                "WITH game, COUNT(DISTINCT review) AS reviewCount" +
-                "MATCH (game)<-[:OWNS]-(owner:User)"+
-                "WHERE owner IN friends"+
-                "WITH game, COUNT(DISTINCT owner) AS friendsOwning"+
-                "RETURN game, reviewCount + friendsOwning*2 AS totalScore"
+                """
+                MATCH (u:User {id: %d})-[:FRIEND]-(friend:User)
+                WITH u, collect(DISTINCT friend) AS friends)
+                MATCH (reviewer:User)-[HAS_WROTE]->(r:Review)-[HAS_REVIEW]->(game:Game)
+                WHERE reviewer IN friends
+                WITH game, COUNT(DISTINCT r) AS reviewCount
+                MATCH (owner:User)-[:OWNS]->(game1:Game)
+                WHERE owner IN friends
+                WITH game, COUNT(DISTINCT owner) AS friendsOwning
+                RETURN game, reviewCount + friendsOwning*2 AS totalScore
+                """
                 ,
                 uid);
         neo4j.executeWriteTransactionQuery(query);
